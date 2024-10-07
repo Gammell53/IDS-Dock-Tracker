7fdfa6ae6a8c9bd88622d4f79568d0706cb08f3a
alexg <gammell53@gmail.com>
2024-10-07 12:02:25 -0500

    DockTracker V1.5

--- next.config.js
+++ next.config.js
@@ -17,17 +17,6 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
