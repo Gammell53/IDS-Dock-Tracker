3d1a711d227f1da74d31349fc931ee77c2cca074
Emil Sabri <emil.sabri@gmail.com>
2024-10-08 21:22:51 -0700

    latest changes

--- deploy.sh
+++ deploy.sh
@@ -1,60 +1,34 @@
 #!/bin/bash
 
 # Configuration
-REPO_DIR="/var/www/IDS-Dock-Tracker"
 GITHUB_REPO="https://github.com/Gammell53/IDS-Dock-Tracker.git"
 BRANCH="nodejs"
 SERVER_IP="209.38.75.55"
 SERVER_USER="root"
 UPLOAD_PATH="/home/ids-deploy"
 
-# Build frontend
-echo "Building frontend..."
-cd frontend
-npm i
-npm run build
-cd ..
-
-# Build backend
-echo "Building backend..."
-cd backend
-npm i
-bun run build --target=bun
-cd ..
-
-# Build Docker images
-echo "Building Docker images..."
-docker-compose build
-
-# Save images to tar files
-echo "Saving Docker images..."
-docker save ids-dock-tracker_frontend > frontend_image.tar
-docker save ids-dock-tracker_backend > backend_image.tar
-
-# Transfer images to server
-echo "Transferring Docker images to server..."
-scp frontend_image.tar backend_image.tar $SERVER_USER@$SERVER_IP:$UPLOAD_PATH
-
-# SSH into server and load images
-echo "Loading Docker images on server..."
+# Pull from GitHub repository
+echo "Pulling from GitHub repository..."
 ssh $SERVER_USER@$SERVER_IP << EOF
     cd $UPLOAD_PATH
-    docker load < frontend_image.tar
-    docker load < backend_image.tar
-    rm frontend_image.tar backend_image.tar
+    git pull origin $BRANCH
+    if [ $? -ne 0 ]; then
+        git clone -b $BRANCH $GITHUB_REPO .
+    fi
+EOF
 
-    # Stop and remove existing containers
-    docker-compose down
+# SSH into server and perform deployment steps
+echo "Performing deployment steps on server..."
+ssh $SERVER_USER@$SERVER_IP << EOF
+    cd $UPLOAD_PATH
 
-    # Start new containers
+    # Build Docker images and run docker-compose
+    echo "Building and running Docker containers..."
+    docker-compose build
     docker-compose up -d
 
     # Clean up old images
     docker image prune -f
 EOF
-
-# Clean up local tar files
-echo "Cleaning up local files..."
-rm frontend_image.tar backend_image.tar
 
 echo "Deployment complete!"
